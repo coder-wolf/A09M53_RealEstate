b6becd723986cedb5650a1f6c43461683189d8f8
coder-wolf <sabir.cse5.bu@gmail.com>
2024-08-27 12:41:28 +0600

    feat: add react helmet to allow dynamic title.

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import router from './Routes/Routes.jsx'
 import { RouterProvider } from 'react-router-dom'
 import AppProvider from './providers/AppProvider.jsx'
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppProvider>
-      <RouterProvider router={router} />
-    </AppProvider>
-  </StrictMode>,
+    <HelmetProvider>
+      <Helmet>
+        <title>Estatery</title>
+      </Helmet>
+      <AppProvider>
+        <RouterProvider router={router} />
+      </AppProvider>
+    </HelmetProvider>
+  </StrictMode>
 )
--- src/pages/Favourites/Favourites.jsx
+++ src/pages/Favourites/Favourites.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react';
 import ListingCard from '../Listing/ListingCard';
 import { ApiContext } from '../../providers/ApiProvider';
+import { Helmet } from 'react-helmet-async';
 
 const Favourites = () => {
     const {
@@ -15,10 +16,13 @@ const Favourites = () => {
 
     return (
         <div className='px-32 bg-[#F1F1F8] pb-32'>
-            <h2 className="text-3xl font-semibold pt-10 pb-12">Favourites</h2>
+            <Helmet>
+                <title>Favourites</title>
+            </Helmet>
+            <h2 className="text-3xl font-semibold pt-10 pb-8">Favourites ({favHouses.length})</h2>
             <div className='grid grid-cols-3 gap-5'>
                 {
-                    favHouses.map(house => <ListingCard house={house}></ListingCard>)
+                    favHouses.map((house, index) => <ListingCard key={index} house={house}></ListingCard>)
                 }
             </div>
         </div>

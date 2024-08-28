9fbabd894b4ef49e9af3e7f5432e15ac96fb25b2
coder-wolf <sabir.cse5.bu@gmail.com>
2024-08-28 12:20:37 +0600

    feat: make property details page functional

--- src/pages/HouseDetails/HouseDetails.jsx
+++ src/pages/HouseDetails/HouseDetails.jsx
@@ -8,18 +8,21 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 const HouseDetails = () => {
     const navigate = useNavigate();
-    const { house } = useLocation().state;
-    // const {
-    //     id,
-    //     estate_title,
-    //     image_url,
-    //     price,
-    //     status,
-    //     bedrooms,
-    //     bathrooms,
-    //     area,
-    //     location,
-    // } = house;
+    const house = useLocation().state;
+    const {
+        id,
+        estate_title,
+        image_url,
+        price,
+        status,
+        buy_rent,
+        bedrooms,
+        bathrooms,
+        area,
+        property_type,
+        location,
+        description,
+    } = house;
 
     return (
         <div className='lg:px-32 md:px-16 px-8 border-t mt-1 pt-8 mb-20'>
@@ -33,23 +36,29 @@ const HouseDetails = () => {
             <div>
                 <div className='flex lg:flex-row flex-col gap-2 lg:justify-between lg:items-end'>
                     <div className='flex lg:flex-col lg:gap-1 gap-3 items-end md:items-start'>
-                        <h2 className='font-semibold text-3xl'>St. Cristal</h2>
-                        <span className='text-gray-400'>1234 Maple Street, Sunnyvale, CA</span>
+                        <h2 className='font-semibold text-3xl'>{estate_title}</h2>
+                        <span className='text-gray-400'>{location}</span>
                     </div>
                     <div className='flex flex-row gap-2 '>
                         {/* <button className="btn border-[#7065F050] text-[#7065F0] bg-[#F7F7FC]"> <FaShare className='text-lg'></FaShare> Share</button> */}
-                        <button onClick={() => {
+                        <button
+                            onClick={() => {
 
-                        }} className="btn border-[#7065F050] text-[#7065F0] bg-[#F7F7FC]"> <CgHeart className='text-lg'></CgHeart> Favorite</button>
-                        <button className="btn border-[#7065F050] text-[#7065F0] bg-[#F7F7FC]"> <BiSearch className='text-lg'></BiSearch> Browse nearby listings</button>
+                            }}
+                            className="btn border-[#7065F050] text-[#7065F0] bg-[#F7F7FC]"> <CgHeart className='text-lg'></CgHeart> Favorite</button>
+                        <button
+                            onClick={() => {
+                                navigate("/listing");
+                            }}
+                            className="btn border-[#7065F050] text-[#7065F0] bg-[#F7F7FC]"> <BiSearch className='text-lg'></BiSearch> Browse nearby listings</button>
                     </div>
                 </div>
             </div>
             <div className='grid grid-cols-3 grid-rows-2 gap-4 mt-8 mb-4'>
                 <img
-                    src="https://cdn.houseplansservices.com/content/h0rig2dbr8vsg0fcgqco7acmul/w991x660.jpg?v=9"
+                    src={image_url}
                     alt=""
-                    className='col-span-2 row-span-2 rounded-lg h-full'
+                    className='col-span-2 row-span-2 rounded-lg w-full object-cover aspect-[3/2.1]'
                 />
                 <img
                     src="https://cdn.houseplansservices.com/content/h0rig2dbr8vsg0fcgqco7acmul/w991x660.jpg?v=9"
@@ -65,49 +74,50 @@ const HouseDetails = () => {
             <div className='grid md:grid-cols-3  gap-4'>
                 <div className='col-span-2'>
                     <div className='w-full lg:h-28 border rounded-lg items-center justify-between px-6 lg:flex lg:flex-row grid md:grid-cols-3 grid-cols-2 pr-16 gap-4 py-5'>
-                        <div className='my-auto lg:h-28 flex flex-col justify-center gap-1'>
+                        <div className='my-auto flex flex-col justify-center gap-1'>
                             <span className='text-gray-500 font-semibold'>Bedrooms</span>
                             <div className='flex text-gray-500 items-center gap-1'>
                                 <BiBed></BiBed>
-                                <span className='font-semibold text-black'>4</span>
+                                <span className='font-semibold text-black'>{bedrooms}</span>
                             </div>
                         </div>
-                        <div className='my-auto lg:h-28 flex flex-col justify-center gap-1'>
+                        <div className='my-auto flex flex-col justify-center gap-1'>
                             <span className='text-gray-500 font-semibold'>Bathrooms</span>
                             <div className='flex text-gray-500 items-center gap-1'>
                                 <FaShower></FaShower>
-                                <span className='font-semibold text-black'>2</span>
+                                <span className='font-semibold text-black'>{bathrooms}</span>
                             </div>
                         </div>
-                        <div className='my-auto lg:h-28 flex flex-col justify-center gap-1'>
+                        <div className='my-auto flex flex-col justify-center gap-1'>
                             <span className='text-gray-500 font-semibold'>Square Area</span>
                             <div className='flex text-gray-500 items-center gap-1'>
                                 <BiArea></BiArea>
-                                <span className='font-semibold text-black'>6<small>x</small>4 m<sup>2</sup></span>
+                                <span className='font-semibold text-black'>{area}<small>sqft</small></span>
                             </div>
                         </div>
-                        <div className='my-auto lg:h-28 flex flex-col justify-center gap-1'>
+                        <div className='my-auto flex flex-col justify-center gap-1'>
+                            <span className='text-gray-500 font-semibold'>Status</span>
+                            <div className='flex text-gray-500 items-center gap-1'>
+                                <HiCheckCircle className='text-green-600'></HiCheckCircle>
+                                <span className='font-semibold text-black'>Available</span>
+                            </div>
+                        </div>
+                        <div className='my-auto flex flex-col justify-center gap-1 col-span-2'>
                             <span className='text-gray-500 font-semibold'>Repair Quality</span>
                             <div className='flex text-gray-500 items-center gap-1'>
                                 <MdHomeRepairService></MdHomeRepairService>
-                                <span className='font-semibold text-black'>Modern Loft</span>
-                            </div>
-                        </div>
-                        <div className='my-auto lg:h-28 flex flex-col justify-center gap-1'>
-                            <span className='text-gray-500 font-semibold'>Status</span>
-                            <div className='flex text-gray-500 items-center gap-1'>
-                                <HiCheckCircle></HiCheckCircle>
-                                <span className='font-semibold text-black'>Active</span>
+                                <span className='font-semibold text-black'>{property_type}</span>
                             </div>
                         </div>
                     </div>
                     <h2 className='text-xl pt-8 pb-2 font-semibold '>About this home</h2>
-                    <p className='text-gray-700'>Check out that Custom Backyard Entertaining space! 3237sqft, 4 Bedrooms, 2 Bathrooms house on a
+                    <p className="text-gray-700">{description}</p>
+                    {/* <p className='text-gray-700'>Check out that Custom Backyard Entertaining space! 3237sqft, 4 Bedrooms, 2 Bathrooms house on a
                         Lake Villa street in the Palm Harbor neighborhood of Texas. Well cared for with tons of upgrades! Newer
                         stainless steel appliances will stay with the unit, including dishwasher, fridge, stove, microwave, and
                         washer and dryer. Tenant pays electricity and gas bills. Water, Sewer, and Trash are covered by
                         Landlord. Tenant is responsible for lawncare and snow removal. Landlord provides lawn mower.
-                        Minimum one year lease.</p>
+                        Minimum one year lease.</p> */}
                     <div className='bg-[#F7F7FC] gap-5 w-full border rounded-lg mt-10 p-5 pt-6 flex flex-col justify-between'>
                         <p className='text-gray-400 font-medium'>Listed by property owner</p>
                         <div className='md:flex grid gap-4 justify-between items-center'>
@@ -134,10 +144,10 @@ const HouseDetails = () => {
                 </div>
                 <div className='border rounded-lg p-4 col-span-2 md:col-span-1'>
                     <div>
-                        <p className='mb-2 text-gray-400'>Rent price</p>
+                        <p className='mb-2 text-gray-400'>{buy_rent} price</p>
                         <p>
-                            <span className='text-[#7065EF] font-semibold text-3xl'>$2,095</span>
-                            <span className='text-gray-400'>/month</span>
+                            <span className='text-[#7065EF] font-semibold text-3xl'>${price}</span>
+                            <span className='text-gray-400'>{buy_rent == "Rent" ? "/month" : ""}</span>
                         </p>
                         <button className='w-full bg-[#7065EF] py-[10px] text-white rounded-lg flex flex-row items-center justify-center gap-1 mt-6 mb-4'>
                             <BiArea></BiArea>
